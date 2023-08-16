6638114c6645c1d32219b12d7d695c6b0f2ea5ac
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-08-15 18:31:27 -0700

    [Add] text in english

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -34,15 +34,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val context = LocalContext.current
-            val store = FavoriteStore(context)
-            val tokenText = store.getFavorites.collectAsState(initial = "")
-            MobileTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    SushiList(SampleData.conversationSample, store)
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val context = LocalContext.current
+    val store = FavoriteStore(context)
+    val tokenText = store.getFavorites.collectAsState(initial = "")
+    MobileTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            val (selectedPage, setSelectedPage) = remember { mutableStateOf(Pages.Landing) }
+            Scaffold(
+                topBar = { TopBar()},
+                bottomBar = { BottomBar(selectedPage, setSelectedPage) }
+            ) { _ ->
+                Column(modifier = Modifier.padding(bottom = 100.dp)) {
+                    if (selectedPage == Pages.SushiList) SushiList(SampleData.conversationSample, store)
+                    else if (selectedPage == Pages.Create) Text(text = "La page création est affichée")
+                    else if (selectedPage == Pages.Favorites) {
+                        Text(text = stringResource(R.string.landing_page))
+                        Text(text = "Les données stockée : ${tokenText.value}")
+                    }
+                    else if (selectedPage == Pages.IngredientList) Text(text = "La page liste d'ingrédients est affichée")
+                    else Landing()
                 }
             }
         }
@@ -53,30 +73,10 @@ class MainActivity : ComponentActivity() {
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
-    name = "Dark Mode"
+    name = "Dark Mode",
+    locale = "en"
 )
 @Composable
 fun PreviewSushiList() {
-    val context = LocalContext.current
-    val store = FavoriteStore(context)
-    val tokenText = store.getFavorites.collectAsState(initial = "")
-
-    MobileTheme {
-        val (selectedPage, setSelectedPage) = remember { mutableStateOf(Pages.Landing) }
-        Scaffold(
-            topBar = { TopBar()},
-            bottomBar = { BottomBar(selectedPage, setSelectedPage) }
-        ) { padding ->
-            Column(modifier = Modifier.padding(bottom = 100.dp)) {
-                if (selectedPage == Pages.SushiList) SushiList(SampleData.conversationSample, store)
-                else if (selectedPage == Pages.Create) Text(text = "La page création est affichée")
-                else if (selectedPage == Pages.Favorites) {
-                    Text(text = stringResource(R.string.landing_page))
-                    Text(text = "Les données stockée : ${tokenText.value}")
-                }
-                else if (selectedPage == Pages.IngredientList) Text(text = "La page liste d'ingrédients est affichée")
-                else Landing()
-            }
-        }
-    }
+    App()
 }
--- app/src/main/java/com/example/mobile/topBar/TopBar.kt
+++ app/src/main/java/com/example/mobile/topBar/TopBar.kt
@@ -1,7 +1,12 @@
 package com.example.mobile.topBar
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,18 +14,25 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.mobile.ui.theme.MobileTheme
 import com.example.mobile.R
+import com.example.mobile.bottomBar.getIconSize
+import com.example.mobile.bottomBar.getWeight
+import java.util.Locale
 
 @Preview(name = "Light Mode")
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
-    name = "Dark Mode"
+    name = "Dark Mode",
+    locale = "en"
 )
 @Composable
 fun PreviewTopBar() {
